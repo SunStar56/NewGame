800ab81932d901dffafa5899507311d37b6f9316
jtp5 <league@WTS22.attlocal.net>
2017-11-26 14:27:30 -0800

    Rooms Update 2
    Added in a title screen(white on black) and a template for the first
    room(red).  Need to finish the second, third, and end screens, and get
    instructions on the title page.
    10/26/17
    
    Bloop. ;)

--- src/Rooms/Rooms.java
+++ src/Rooms/Rooms.java
@@ -1,25 +1,29 @@
+package Rooms;
+
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 public class Rooms {
 	JFrame frame;
 	static final int FRAME_WIDTH = java.awt.Toolkit.getDefaultToolkit().getScreenSize().width;
 	static final int FRAME_HEIGHT = java.awt.Toolkit.getDefaultToolkit().getScreenSize().height;
-	JPanel panel;
-	
+	RoomsPanel panel;
+
 	public Rooms() {
 		frame = new JFrame();
-		panel = new JPanel();	
+		panel = new RoomsPanel();
 		setup();
 	}
+
 	public static void main(String[] args) {
 		Rooms r = new Rooms();
 	}
+
 	void setup() {
 		frame.add(panel);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+		panel.startGame();
 		frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
-		
+		frame.addKeyListener(panel);
 	}
 }
---
+++ src/Rooms/RoomsPanel.java
@@ -0,0 +1,191 @@
+package Rooms;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class RoomsPanel extends JPanel implements ActionListener, KeyListener, MouseListener {
+	Timer timer;
+	final int TITLE_STATE = 0;
+	final int ROOM_ONE = 1;
+	final int ROOM_TWO = 2;
+	final int ROOM_THREE = 3;
+	final int END_STATE = 4;
+	int state = TITLE_STATE;
+	final int LBOUNDSX = 50;
+	final int LBOUNDSY = 500;
+	final int RBOUNDSX = 1820;
+	final int RBOUNDSY = 500;
+	Font titleFontLarge;
+	JLabel LArrow;
+	JLabel RArrow;
+
+	public void paintComponent(Graphics g) {
+		if (state == TITLE_STATE) {
+			drawTitleState(g);
+		} else if (state == ROOM_ONE) {
+			drawRoomOne(g);
+		} else if (state == ROOM_TWO) {
+			drawRoomTwo(g);
+		} else if (state == ROOM_THREE) {
+			drawRoomThree(g);
+		} else if (state == END_STATE) {
+			drawEndState(g);
+		}
+
+	}
+
+	public RoomsPanel() {
+		timer = new Timer(1000 / 60, this);
+		titleFontLarge = new Font("Arial", Font.PLAIN, 48);
+		LArrow = new JLabel("Left(Insert Icon)");
+		RArrow = new JLabel("Right(Insert Icon)");
+		LArrow.addMouseListener(this);
+		RArrow.addMouseListener(this);
+	}
+
+	void startGame() {
+		timer.start();
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		repaint();
+		if (state == TITLE_STATE) {
+			updateTitleState();
+		} else if (state == ROOM_ONE) {
+			updateRoomOne();
+		} else if (state == ROOM_TWO) {
+			updateRoomTwo();
+		} else if (state == ROOM_THREE) {
+			updateRoomThree();
+		} else if (state == END_STATE) {
+			updateEndState();
+		}
+	}
+
+	void drawTitleState(Graphics g) {
+		// TODO Auto-generated method stub
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, Rooms.FRAME_WIDTH + 50, Rooms.FRAME_HEIGHT);
+		g.setFont(titleFontLarge);
+		g.setColor(Color.WHITE);
+		g.drawString("ROOMS", 900, 540);
+		System.out.println(Rooms.FRAME_HEIGHT);
+		System.out.println(Rooms.FRAME_WIDTH);
+		g.setColor(Color.GRAY);
+		LArrow.setVisible(true);
+		RArrow.setVisible(false);
+	}
+
+	void drawRoomOne(Graphics g) {
+		g.setColor(Color.RED);
+		g.fillRect(0, 0, Rooms.FRAME_WIDTH, Rooms.FRAME_HEIGHT);
+	}
+
+	void drawRoomTwo(Graphics g) {
+		// TODO Auto-generated method stub
+
+	}
+
+	void drawRoomThree(Graphics g) {
+		// TODO Auto-generated method stub
+
+	}
+
+	void drawEndState(Graphics g) {
+		// TODO Auto-generated method stub
+
+	}
+
+	void updateTitleState() {
+		// TODO Auto-generated method stub
+	}
+
+	void updateRoomOne() {
+
+	}
+
+	void updateRoomTwo() {
+
+	}
+
+	void updateRoomThree() {
+
+	}
+
+	void updateEndState() {
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			state -= 1;
+			if (state < 0) {
+				state = 4;
+			}
+			System.out.println(state);
+		} else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			state += 1;
+			if (state > 4) {
+				state = 0;
+			}
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		if (mouse) != null) {
+			System.out.println("Beep");
+			state += 1;
+		}
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+}
